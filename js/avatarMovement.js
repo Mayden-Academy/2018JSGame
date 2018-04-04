53128565cc15adb1de12604775513f46f79f39c7
Karim Kefi <karimkefi@gmail.com>
2018-04-04 16:57:34 +0100

    dual movement on continuous hold

--- js/avatarMovement.js
+++ js/avatarMovement.js
@@ -16,8 +16,8 @@ var playerA = drawPlayer(playerAx, playerAy, playerAHeight, playerAWidth, 'pink'
 var playerB = drawPlayer(playerBx, playerBy, playerBHeight, playerBWidth, 'blue', 'playerB');
 
 
-var distance = 50;
-var time = 40;
+var distance = 1000;
+var time = 3000;
 
 
 function drawPlayer (x, y, h, w, fill, id) {
@@ -36,31 +36,33 @@ function drawPlayer (x, y, h, w, fill, id) {
 //  <-  37 = left arrow,  39 = right arrow , 38 = up arrow, 40 = down arrow
 //  <-  65 = A (left) ,  68 = D (right)  , 87 = W (up) , 83 = S (down)
 function keyDownHandler(e) {
+    $playerA=$(playerA);
+    $playerB=$(playerB);
     switch(e.keyCode) {
         case 37:
-            $(playerA).animate({x: "-=" + distance}, time ,'linear');
+            moveLeft($playerA);
             break;
         case 39:
-            $(playerA).animate({x: "+=" + distance}, time ,'linear');
+            moveRight($playerA);
             break;
         case 38:
-            $(playerA).animate({y: "-=" + distance}, time ,'linear');
+            moveUp($playerA);
             break;
         case 40:
-            $(playerA).animate({y: "+=" + distance}, time ,'linear');
+            moveDown($playerA);
             break;
 
         case 65:
-            $(playerB).animate({x: "-=" + distance}, time ,'linear');
+            moveLeft($playerB);
             break;
         case 68:
-            $(playerB).animate({x: "+=" + distance}, time ,'linear');
+            moveRight($playerB);
             break;
         case 87:
-            $(playerB).animate({y: "-=" + distance}, time ,'linear');
+            moveUp($playerB);
             break;
         case 83:
-            $(playerB).animate({y: "+=" + distance}, time ,'linear');
+            moveDown($playerB);
             break;
         default:
             downPressed = false;            //switch needs a default.. not sure what this should be??
@@ -68,7 +70,68 @@ function keyDownHandler(e) {
 }
 
 
+function keyUpHandler(e) {
+    switch(e.keyCode) {
+        case 37:
+        case 39:
+        case 38:
+        case 40:
+            stopMovement(playerA);
+            break;
+        case 65:
+        case 68:
+        case 87:
+        case 83:
+            stopMovement(playerB);
+            break;
+
+        default:
+            downPressed = false;            //switch needs a default.. not sure what this should be??
+    }
+}
+
+
+function moveLeft($player) {
+    if (!$player.hasClass('moving')) {
+        console.log('start');
+        $player.addClass('moving');
+        $player.animate({x: "-="+distance}, time ,'linear');
+    }
+}
+
+function moveRight($player) {
+    if (!$player.hasClass('moving')) {
+        console.log('start');
+        $player.addClass('moving');
+        $player.animate({x: "+="+distance}, time ,'linear');
+    }
+}
+
+function moveUp($player) {
+    if (!$player.hasClass('moving')) {
+        console.log('start');
+        $player.addClass('moving');
+        $player.animate({y: "-="+distance}, time ,'linear');
+    }
+}
+
+function moveDown($player) {
+    if (!$player.hasClass('moving')) {
+        console.log('start');
+        $player.addClass('moving');
+        $player.animate({y: "+="+distance}, time ,'linear');
+    }
+}
+
+
+
+function stopMovement (player){
+    $(player).removeClass('moving');
+    $(player).stop()
+}
+
+
 //event listener for player's avatar
 document.addEventListener("keydown", keyDownHandler, false);
-
+document.addEventListener("keyup", keyUpHandler, false);
 

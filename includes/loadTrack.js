d2696a02e9511bfc0fc51f3c2d878df2e1587d69
Nick Santiago Blair <nsantiagoblair@gmail.com>
2018-04-04 11:58:30 +0100

    refactor backTrack.js onclick

--- includes/loadTrack.js
+++ includes/loadTrack.js
@@ -1,30 +1,9 @@
-// 1 We want to select 2 links ON CLICK
+
 $('.trackLink').click(function(e) {
-    // console.log(e.target)
-    var splash = $('#splash')
+    var splash = $('#splash'),
+        container = $('#container')
+        
     splash.css('display', 'none')
-    // console.log(splash)
-    var container = $('#container')
-    console.log(container)
     container.load('includes/track1.html');
 })
 
-
-
-// 2 If AJAX is successful hide the splash screen
-
-// 3 Make an AJAX request
-
-// 4 If AJAX is sucessful display track
-
-// 5 If AJAX unsuccessful keep splash screen
-
-
-// $('#pullInstructions').click(function() {
-//     $('#instructions').load('instructions.html');
-//     $('#instructions').addClass('instructionsShow');
-// });
-
-// $('body').on('click', '#instructionsClose' , function() {
-//     $('#instructions').removeClass('instructionsShow');
-// });

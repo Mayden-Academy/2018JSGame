5784f31b3964a983d9b3a16eef5662dfc8122fc4
Nick Santiago Blair <nsantiagoblair@gmail.com>
2018-04-04 10:44:03 +0100

    select level anchor tags on click

---
+++ includes/loadTrack.js
@@ -0,0 +1,24 @@
+// 1 We want to select 2 links ON CLICK
+$('.trackLink').click(function(e) {
+    console.log(e.target)
+})
+
+
+
+// 2 If AJAX is successful hide the splash screen
+
+// 3 Make an AJAX request
+
+// 4 If AJAX is sucessful display track
+
+// 5 If AJAX unsuccessful keep splash screen
+
+
+// $('#pullInstructions').click(function() {
+//     $('#instructions').load('instructions.html');
+//     $('#instructions').addClass('instructionsShow');
+// });
+
+// $('body').on('click', '#instructionsClose' , function() {
+//     $('#instructions').removeClass('instructionsShow');
+// });

c6270c9d89c45794659207d52cd360240e61ee1d
C0deJack <jackwoodpearce@gmail.com>
2018-04-06 10:19:02 +0100

    created a listener for the winner pop up

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,26 @@
-$('.gameLevel').on('collision', function (player) {
-    //hardcoding the winner - to be passed as part of the collision event
-    var player = 'Charlie'
-    winnerPopup(player)
-})
+$('#playerA').on('death', deathCheck(playerA, playerB))
+$('#playerB').on('death',deathCheck(playerA, playerB))
 
-//=======the above is to fire the event  (change collision to click for code review) =========
+function deathCheck(playerA, playerB) {
+    if ( playerA === 'dead'  ) {
+        return playerB;
+    } else if  ( playerB === 'dead'  ){
+        return playerA;
+    }
+}
+
+// function reachedFinishLine(playerA, playerB) {
+//     x="960" y="680" width="40" height="100"
+//     x="950" y="700" width="100" height="100"
+// }
+
+function callWinner (playerA, playerB) {
+    pauseGame('playerA', 'playerB')
+    var winnerByDeathCheck = deathCheck(playerA, playerB)
+    var winnerByRace = reachedFinishLine(playerA, playerB)
+    winnerPopup(winnerByDeathCheck)
+    winnerPopup(winnerByRace)
+}
 
 $('body').on('click', '#btnRestart' , function() {
     $('#restart').removeClass('restartShow')
